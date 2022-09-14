254a035957fade372e68273790ce388e5927f48c
Adrien <adrien.barguil@protonmail.com>
2022-09-14 17:01:25 +0200

    hero v2.0

--- client/src/assets/index.js
+++ client/src/assets/index.js
@@ -1,9 +1,11 @@
 import logo from './logoChef.svg'
 import heroPic1 from './heroPic1.jpg';
 import heroPic2 from './heroPic2.jpg';
+import heroBg from './heroBg.jpg';
 
 export {
     logo,
     heroPic1,
-    heroPic2
+    heroPic2,
+    heroBg
 }
--- client/src/components/Hero/Hero.jsx
+++ client/src/components/Hero/Hero.jsx
@@ -17,7 +17,7 @@ const Hero = () => {
                     </div>
                 </div>
                 */}
-                <div className="carte">
+                {/*<div className="carte">
                     <div className="leftContainer">
                         <div>
                             <img src={logo} alt="" />
@@ -36,6 +36,24 @@ const Hero = () => {
                             </div>
                         </div>
                     </div>
+                </div>*/}
+                <div className='logoContainer'>
+                    <img src={logo} alt="" />
+                    <h1>Nos-recettes.fr</h1>
+                </div>
+                <div className="cta">
+                    <a href="">
+                        Rechercher
+                    </a>
+                    <a href="">
+                        Comment ça marche ?
+                    </a>
+                    <a href="">
+                        Se connecter
+                    </a>
+                    <a href="">
+                        S'inscrire
+                    </a>
                 </div>
             </div>
         </div>
